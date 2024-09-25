e5b9009e9cff1bf4fa891dbff4bb674c4e085503
BlairTodd17 <blairtodd353@gmail.com>
2024-09-25 19:43:03 -0400

    Update Chapter 2 project.cpp

--- Chapter 2 project/Chapter 2 project.cpp
+++ Chapter 2 project/Chapter 2 project.cpp
@@ -1,14 +1,1 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    const double risingRate = 1.5; 
-    int years[] = { 5, 7, 10 };
-
-    for (int year : years) {
-        double increase = risingRate * year;
-        cout << "In " << year << " years, the ocean level will rise by " << increase << " millimeters." << endl;
-    }
-
-    return 0;
-}
+//code in its own branch
