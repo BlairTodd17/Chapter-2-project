4c2befd8691b9c32a7e83a2f99aa7db3f2ffaf07
Blair Todd <bjbbt4@outlook.com>
2024-09-25 19:25:19 -0400

    we

--- Chapter 2 project/Chapter 2 project.cpp
+++ Chapter 2 project/Chapter 2 project.cpp
@@ -1,1 +1,16 @@
-//no code here
+#include <iostream>
+using namespace std;
+
+int main() {
+    const double tankSize = 20.0; // gallons
+    const double townMpg = 23.5;
+    const double highwayMpg = 28.9;
+
+    double townDistance = tankSize * townMpg;
+    double highwayDistance = tankSize * highwayMpg;
+
+    cout << "Distance in town: " << townDistance << " miles" << endl;
+    cout << "Distance on highway: " << highwayDistance << " miles" << endl;
+
+    return 0;
+}
