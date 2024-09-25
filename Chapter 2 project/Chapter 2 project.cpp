68819a36aaadbff869c11729850ea86e306c63c1
Blair Todd <bjbbt4@outlook.com>
2024-09-25 19:26:35 -0400

    qqq

--- Chapter 2 project/Chapter 2 project.cpp
+++ Chapter 2 project/Chapter 2 project.cpp
@@ -1,1 +1,16 @@
-//no code here
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int totalCustomers = 16500;
+    const double percentPurchase = 0.15;
+    const double percentCitrus = 0.58;
+
+    int customersPurchasing = totalCustomers * percentPurchase;
+    int customersCitrus = customersPurchasing * percentCitrus;
+
+    cout << "Customers purchasing energy drinks: " << customersPurchasing << endl;
+    cout << "Customers preferring citrus-flavored drinks: " << customersCitrus << endl;
+
+    return 0;
+}
